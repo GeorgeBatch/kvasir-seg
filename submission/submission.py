f50e833baf9ebde856de3d20b7aaad8ae4de24a1
GeorgeBatch <george.batchkala@gmail.com>
2020-11-16 01:23:21 +0000

    produce submission file

---
+++ submission/submission.py
@@ -0,0 +1,177 @@
+# Import modules
+import os
+import time
+
+import numpy as np
+
+import cv2
+import imageio
+import torchvision.transforms as transforms
+
+import torch
+
+
+# ----------------------------------------------------------------------------
+# select device for training, define how we resize
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+_size = 256, 256
+resize = transforms.Resize(_size, interpolation=0)
+
+
+# ----------------------------------------------------------------------------
+# Model.
+
+class UNet(torch.nn.Module):
+
+    def conv_block(self, channel_in, channel_out):
+        return torch.nn.Sequential(
+            torch.nn.Conv2d(channel_in, channel_out, kernel_size=3, padding=1),
+            torch.nn.BatchNorm2d(channel_out),
+            torch.nn.ReLU(inplace=True),
+            torch.nn.Conv2d(channel_out, channel_out, kernel_size=3, padding=1),
+            torch.nn.BatchNorm2d(channel_out),
+            torch.nn.ReLU(inplace=True)
+        )
+
+
+    def __init__(self, channel_in, channel_out, bilinear=None):
+        super(UNet, self).__init__()
+        self.channel_in = channel_in
+        self.channel_out = channel_out
+
+        # initial convolutional block
+        self.initial = self.conv_block(channel_in, 64)
+
+        # encoder layers
+        self.down0 = self.conv_block(64, 128)
+        self.down1 = self.conv_block(128, 256)
+        self.down2 = self.conv_block(256, 512)
+        self.down3 = self.conv_block(512, 1024)
+
+        # decoder layers
+        self.up0_0 = torch.nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
+        self.up0_1 = self.conv_block(1024, 512)
+        self.up1_0 = torch.nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
+        self.up1_1 = self.conv_block(512, 256)
+        self.up2_0 = torch.nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.up2_1 = self.conv_block(256, 128)
+        self.up3_0 = torch.nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.up3_1 = self.conv_block(128, 64)
+
+        # final layer before output
+        self.final = torch.nn.Conv2d(64, channel_out, kernel_size=1)
+
+    def forward(self,x):
+        "Forward pass"
+        x_in= self.initial(x)
+        enc0 = self.down0(torch.nn.MaxPool2d(2)(x_in))
+        enc1 = self.down1(torch.nn.MaxPool2d(2)(enc0))
+        enc2 = self.down2(torch.nn.MaxPool2d(2)(enc1))
+        enc3 = self.down3(torch.nn.MaxPool2d(2)(enc2))
+
+        dec0 = self.up0_0(enc3)
+        diff0 = torch.FloatTensor(list(enc2.size())[2:]) - torch.FloatTensor(list(dec0.shape))[2:]
+        dec0 = torch.nn.functional.pad(dec0, (int((diff0/2).floor()[0]), int((diff0/2).ceil()[0]), int((diff0/2).floor()[1]), int((diff0/2).ceil()[1])))
+        dec0 = self.up0_1(torch.cat((enc2, dec0), dim=1))
+
+        dec1 = self.up1_0(dec0)
+        diff1 = torch.FloatTensor(list(enc1.size())[2:]) - torch.FloatTensor(list(dec1.shape))[2:]
+        dec1 = torch.nn.functional.pad(dec1, (int((diff1/2).floor()[0]), int((diff1/2).ceil()[0]), int((diff1/2).floor()[1]), int((diff1/2).ceil()[1])))
+        dec1 = self.up1_1(torch.cat((enc1, dec1), dim=1))
+
+        dec2 = self.up2_0(dec1)
+        diff2 = torch.FloatTensor(list(enc0.size())[2:]) - torch.FloatTensor(list(dec2.shape))[2:]
+        dec2 = torch.nn.functional.pad(dec2, (int((diff2/2).floor()[0]), int((diff2/2).ceil()[0]), int((diff2/2).floor()[1]), int((diff2/2).ceil()[1])))
+        dec2 = self.up2_1(torch.cat((enc0, dec2), dim=1))
+
+        dec3 = self.up3_0(dec2)
+        diff3 = torch.FloatTensor(list(x.size())[2:]) - torch.FloatTensor(list(dec3.shape))[2:]
+        dec3 = torch.nn.functional.pad(dec3, (int((diff3/2).floor()[0]), int((diff3/2).ceil()[0]), int((diff3/2).floor()[1]), int((diff3/2).ceil()[1])))
+        dec3 = self.up3_1(torch.cat((x_in, dec3), dim=1))
+
+        x_out = self.final(dec3) # ? no activation here
+        return x_out
+
+
+# ----------------------------------------------------------------------------
+# Instantiate the model and load weights
+
+model_name = 'UNet_IoULoss_baseline'
+
+# Instantiate the model
+model = UNet(channel_in=3, channel_out=1)
+model = model.to(DEVICE) # load model to DEVICE
+
+# load best weights and put into the evaluation mode
+model.load_state_dict(torch.load('ckpt_UNet_IoULoss_baseline.pth')['net'])
+
+
+# ----------------------------------------------------------------------------
+# Set path to test dataset (Like in the instructions)
+TEST_DATASET_PATH = "/medico2020"
+MASK_PATH = "/mask"
+
+
+# ----------------------------------------------------------------------------
+# Inference
+
+time_taken = []
+
+model.eval() # enter inference/evaluation mode
+for name in os.listdir(TEST_DATASET_PATH):
+    path_img = os.path.join(TEST_DATASET_PATH, name)
+
+    img = imageio.imread(path_img) / 255
+
+    # record shape to revert to
+    H, W, _ = img.shape
+    resize_back = transforms.Resize((H, W), interpolation=0)
+
+    # convert to Tensors and fix the dimentions (Pytorch uses the channels in the first dimension)
+    img = torch.FloatTensor(np.transpose(img, [2, 0 ,1])).unsqueeze(0)
+
+    # resize for the model
+    img = resize(img)
+
+    # put on the GPU
+    img = img.to(DEVICE)
+
+    # we do not need to calculate gradients
+    with torch.no_grad():
+        # Start time
+        start_time = time.time()
+        ## Prediction
+        pred = model(img)
+        # End timer
+        end_time = time.time() - start_time
+
+    time_taken.append(end_time)
+    print("{} - {:.10f}".format(name, end_time))
+
+
+    # resize back, nearest interpolation, since it's a mask
+    pred = resize_back(pred)
+    # put on cpu
+    pred = pred.cpu()
+
+    # remove channel: BATCH x 1 x H x W => BATCH x H x W
+    pred = pred.squeeze(1)
+
+    # comment out if your model contains a sigmoid or equivalent activation layer
+    pred = torch.sigmoid(pred)
+    # thresholding since that's how we will make predictions on new imputs
+    pred = pred > 0.5
+    # remove BATCH => H x W
+    pred = pred.squeeze(0)
+    # converto to correct type
+    pred = pred.numpy().astype(np.float32)
+    # revert to standard intensities
+    pred = pred * 255.0
+    # save
+    pred_path = os.path.join(MASK_PATH, name)
+    cv2.imwrite(pred_path, pred)
+
+
+mean_time_taken = np.mean(time_taken)
+mean_fps = 1/mean_time_taken
+print("\nMean FPS: ", mean_fps)
