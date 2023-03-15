eeb1992b89f25451bc760a0925637be067b0de68
George Batchkala <george.batchkala@gmail.com>
2023-03-15 08:59:20 +0000

    fix a bug with different interpolation modes and standardise them to avoid such bugs

--- utils/transforms.py
+++ utils/transforms.py
@@ -1,19 +1,20 @@
 import torchvision.transforms as transforms
 
 SIZE = 256
+INTERPOLATION_MODE = transforms.InterpolationMode.NEAREST
 
-resize_transform = transforms.Resize(SIZE, interpolation=transforms.InterpolationMode.NEAREST)
+resize_transform = transforms.Resize(SIZE, interpolation=INTERPOLATION_MODE)
 
-
+# TODO: check if the padding argument changed since 2020
 train_transforms = transforms.Compose([
-                           transforms.Resize(SIZE, interpolation=transforms.InterpolationMode.NEAREST),
+                           transforms.Resize(SIZE, interpolation=INTERPOLATION_MODE),
                            transforms.RandomRotation(180),
                            transforms.RandomHorizontalFlip(0.5),
                            transforms.RandomCrop(SIZE, padding = 10), # needed after rotation (with original size)
                        ])
 
 test_transforms = transforms.Compose([
-                           transforms.Resize(SIZE, interpolation=transforms.InterpolationMode.BILINEAR),
+                           transforms.Resize(SIZE, interpolation=INTERPOLATION_MODE),
                        ])
 
 
